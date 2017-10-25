ffbf46bac96e65bf265b6ac2673f0e2285af883d
dbrewii <dbrewer5@memphis.edu>
2017-10-25 16:15:37 -0500

    Dropdown added.

--- app/src/main/java/edu/memphis/teamhack/smart_nightlight/MainActivity.java
+++ app/src/main/java/edu/memphis/teamhack/smart_nightlight/MainActivity.java
@@ -11,8 +11,9 @@ import android.view.MenuItem;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-
+import android.widget.Spinner;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -48,7 +49,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
