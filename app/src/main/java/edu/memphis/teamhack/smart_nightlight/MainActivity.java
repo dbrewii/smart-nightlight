8c826377e3bd0f53ff7ca8a5b4a9574b7568e576
dbrewii <dbrewer5@memphis.edu>
2017-11-05 23:26:08 -0600

    Added bluetooth module.

--- app/src/main/java/edu/memphis/teamhack/smart_nightlight/MainActivity.java
+++ app/src/main/java/edu/memphis/teamhack/smart_nightlight/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,11 +16,41 @@ import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import android.widget.ListView;
+import java.util.Set;
+import java.util.ArrayList;
+import android.widget.TextView;
+import android.content.Intent;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+//import android.widget.AdapterView.OnClickListener
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnItemSelectedListener {
     Button button;
+    Button btBtn;
+    ListView deviceList;
+    private BluetoothAdapter myBluetooth = null;
+    private Set pairedDevices;
     @Override
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        String[] planetsArray = getResources().getStringArray(R.array.planets_array);
+        //Toast.makeText(getApplicationContext(),planetsArray[pos], Toast.LENGTH_LONG).show();
+        // An item was selected. You can retrieve the selected item using
+        if(planetsArray[pos].equals("Mercury")){
+            Toast.makeText(getApplicationContext(),"pap",Toast.LENGTH_LONG).show();
+        }
+        if(planetsArray[pos].equals("Venus")){
+            Toast.makeText(getApplicationContext(),"pop",Toast.LENGTH_LONG).show();
+        }
+    }
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+        Log.wtf("wtfTag","yo");
+    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -50,6 +82,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.planets_array, android.R.layout.simple_spinner_item);
@@ -57,6 +91,10 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        //Bluetooth module
+        btBtn = (Button)findViewById(R.id.button);
+        deviceList = (ListView)findViewById(R.id.listView);
 
     }
 
---
+++ arduino/bluetooth_test/bluetooth_test.ino
@@ -0,0 +1,45 @@
+/**
+ * Simple routine that performs the following:
+ *  1. Configures the software UART on pins 2 and 4 (RX,TX)
+ *  2. Increments a 32-bit variable every 500ms
+ *  4. If it receives a '1' character from bluetooth, it toggles an LED
+ *     
+ *  @author Justin Bauer - mcuhq.com
+ *  @date 4.24.2016
+ */
+
+#include <SoftwareSerial.h> // use the software uart
+SoftwareSerial bluetooth(2, 4); // RX, TX
+
+unsigned long previousMillis = 0;        // will store last time
+const long interval = 500;           // interval at which to delay
+static uint32_t tmp; // increment this
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT); // for LED status
+  bluetooth.begin(9600); // start the bluetooth uart at 9600 which is its default
+  delay(200); // wait for voltage stabilize
+  bluetooth.print("Asswipe"); // place your name in here to configure the bluetooth name.
+                                       // will require reboot for settings to take affect. 
+  delay(3000); // wait for settings to take affect. 
+}
+
+void loop() {
+  if (bluetooth.available()) { // check if anything in UART buffer
+    if(bluetooth.read() == '1'){ // did we receive this character?
+       digitalWrite(LED_BUILTIN,HIGH); // if so, toggle the onboard LED
+       
+    }
+    else{
+       digitalWrite(LED_BUILTIN,LOW); // if so, toggle the onboard LED
+
+    }
+  }
+  
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    bluetooth.print(tmp++); // print this to bluetooth module
+  }
+
+}
