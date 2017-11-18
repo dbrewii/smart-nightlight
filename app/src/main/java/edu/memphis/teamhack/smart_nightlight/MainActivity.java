1b7a62974c123c91c7577abe5755e30afd071acc
dbrewii <dbrewer5@memphis.edu>
2017-11-18 14:22:45 -0600

    Added working bluetooth module.

--- app/src/main/java/edu/memphis/teamhack/smart_nightlight/MainActivity.java
+++ app/src/main/java/edu/memphis/teamhack/smart_nightlight/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.memphis.teamhack.smart_nightlight;
 
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,13 +20,19 @@ import android.content.Intent;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.AdapterView;
 import android.widget.Toast;
 import android.widget.ListView;
+
+import java.io.IOException;
 import java.util.Set;
 import java.util.ArrayList;
+import java.util.UUID;
+
 import android.widget.TextView;
 import android.content.Intent;
 import android.bluetooth.BluetoothAdapter;
@@ -38,7 +47,6 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
     Button sendBtn;
 
     ListView deviceList;
-    private BluetoothAdapter myBluetooth = null;
     private Set pairedDevices;
     //private int colorHex=0x345f21;
 
@@ -47,7 +55,17 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
     private TextView mBluetoothStatus;
     private ArrayAdapter<String> mBTArrayAdapter;
 
-    private BluetoothActivity;
+    private BluetoothCommService btCom;
+
+    Button btnOn, btnOff, btnDis;
+    SeekBar brightness;
+    TextView lumn;
+    String address = null;
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConnected = false;
+    //SPP UUID. Look for it
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
 
 
@@ -71,6 +89,16 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
     }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Bluetooth
+        Intent newint = getIntent();
+        address = newint.getStringExtra(BluetoothActivity.EXTRA_ADDRESS); //receive the address of the bluetooth device
+
+        new ConnectBT().execute(); //Call the class to connect
+
+
+
+        //other
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -94,7 +122,8 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         button.setOnClickListener(new OnClickListener() {
             public void onClick(View arg0) {
 
-
+                Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(myIntent);
             }
         });
 
@@ -111,24 +140,18 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
 
         //Bluetooth module
 
-        btBtn.setOnClickListener(new View.OnClickListener() {
+        btBtn.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                //startActivity(new Intent(MainActivity.this, BluetoothActivity.class));
-                Intent serviceIntent = new Intent();
-                serviceIntent.setAction("edu.memphis.teamhack.smart_nightlight.BluetoothDataService.class");
-                startService(serviceIntent);
+            public void onClick(View v)
+            {
+                Disconnect(); //close connection
             }
         });
 
         sendBtn.setOnClickListener(new OnClickListener() {
             public void onClick(View arg0) {
-                //BluetoothActivity.mConnectedThread.write("0x345f21")
-                SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = pref.edit();
-                editor.putInt("colorHex", 0x345f21);
-                editor.commit();
-                //((BluetoothDataService)this.getApplication()).setColorHex(0x345f21);
+                sendParam("COLOR1");
             }
         });
 
@@ -136,6 +159,20 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
 
 
 
+
+    public void sendParam(String param){
+        if (btSocket!=null)
+        {
+            try
+            {
+                btSocket.getOutputStream().write(param.toString().getBytes());
+            }
+            catch (IOException e)
+            {
+                msg("Error");
+            }
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -161,4 +198,74 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
     public void sendSignal(String colorHex){
 
     }
+
+    private void Disconnect()
+    {
+        if (btSocket!=null) //If the btSocket is busy
+        {
+            try
+            {
+                btSocket.close(); //close connection
+            }
+            catch (IOException e)
+            { msg("Error");}
+        }
+        finish(); //return to the first layout
+
+    }
+
+    private void msg(String s)
+    {
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+    }
+
+    private class ConnectBT extends AsyncTask<Void, Void, Void>  // UI thread
+    {
+        private boolean ConnectSuccess = true; //if it's here, it's almost connected
+
+        @Override
+        protected void onPreExecute()
+        {
+            //progress = ProgressDialog.show(ledControl.this, "Connecting...", "Please wait!!!");  //show a progress dialog
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices) //while the progress dialog is shown, the connection is done in background
+        {
+            try
+            {
+                if (btSocket == null || !isBtConnected)
+                {
+                    myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
+                    BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);//connects to the device's address and checks if it's available
+                    btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    btSocket.connect();//start connection
+                }
+            }
+            catch (IOException e)
+            {
+                ConnectSuccess = false;//if the try failed, you can check the exception here
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) //after the doInBackground, it checks if everything went fine
+        {
+            super.onPostExecute(result);
+
+            if (!ConnectSuccess)
+            {
+                msg("Connection Failed. Is it a SPP Bluetooth? Try again.");
+                finish();
+            }
+            else
+            {
+                msg("Connected.");
+                isBtConnected = true;
+            }
+            //progress.dismiss();
+        }
+    }
+
 }
---
+++ arduino/bluetooth_to_blinkm/bluetooth_to_blinkm.ino
@@ -0,0 +1,75 @@
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
+
+#include <SoftwareSerial.h> // use the software uart
+#include <stdint.h>
+SoftwareSerial bluetooth(2, 4); // RX, TX
+
+unsigned long previousMillis = 0;        // will store last time
+const long interval = 500;           // interval at which to delay
+static uint32_t tmp; // increment this
+String temp="";
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+  
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(1000);
+  
+if ((bluetooth.available())) { // check if anything in UART buffer
+    //if(temp.startsWith("COLOR")){
+      temp = bluetooth.read();
+      writeString(temp);
+    //}
+    bluetooth.print("Printing 0x");
+    bluetooth.println(temp);
+}
+
+bluetooth.print("Printing 0x");
+bluetooth.println(temp);
+    unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    bluetooth.print(tmp++); // print this to bluetooth module
+  }
+}
+
+void writeString(String stringData) { // Used to serially push out a String with Serial.write()
+
+  for (int i = 0; i < stringData.length(); i++)
+  {
+    Serial.write(stringData[i]);   // Push each char 1 by 1 on each loop pass
+  }
+
+}// end writeString
